24264eeb221fd5d90c757a45844612de9e7a1833
wyep266 <wesleyyep@hotmail.com>
2014-09-19 10:44:28 +1200

    blank TextWorker

---
+++ src/editing/TextWorker.java
@@ -0,0 +1,13 @@
+package editing;
+
+import javax.swing.SwingWorker;
+
+public class TextWorker extends SwingWorker{
+
+	@Override
+	protected Object doInBackground() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
